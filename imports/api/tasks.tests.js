9384f25c720f99ece77f07a5f1930be18505f2b1
bryanda <bryanda.garcia@gmail.com>
2016-12-02 14:20:57 -0600

    Added test to check delete method

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -2,6 +2,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
 
 import { Tasks } from './tasks.js';
 
@@ -20,8 +21,17 @@ if ( Meteor.isServer) {
                     username: 'tmeasday',
                 });
             });
-            
+
             it('can delete owned task', () => {
+                //find the internal implementation of the task method so we can 
+                //test it in isolation
+                const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+                //Run the method with 'this' set to the fake invocation
+                deleteTask.apply(invocation, [taskId]);
+
+                //Verify that the method does what we expected
+                assert.equal(tasks.find().count(), 0);
             });
         });
     });
